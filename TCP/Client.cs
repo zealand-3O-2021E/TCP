5298e48874acf01f5949de6a930b4e0bce917221
banan <edem0004@edu.easj.dk>
2021-10-14 23:02:26 +0200

    Assignment 3

---
+++ TCP/Client.cs
@@ -0,0 +1,64 @@
+﻿using Library;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP
+{
+    class Client
+    {
+
+        public async Task<string> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                    client.GetAsync("https://localhost:44333/api/Books");
+                return await response.Content.ReadAsStringAsync();
+
+            }
+        }
+        public async Task<string> Get(string isbn)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                       client.GetAsync("https://localhost:44333/api/Books/" + isbn);
+                 return await response.Content.ReadAsStringAsync();
+            }
+        }
+        public async void Post(string book)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(book, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await
+                    client.PostAsync("https://localhost:44333/api/Books/", content);
+            }
+        }
+        public async void Put(string book)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(book, Encoding.UTF8, "application/json");
+                var ISBN = JsonConvert.DeserializeObject<Book>(book).ISBN13;
+                HttpResponseMessage response = await
+                    client.PutAsync("https://localhost:44333/api/Books/" + ISBN, content);
+            }
+        }
+        public async void Remove(string isbn)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                    client.DeleteAsync("https://localhost:44333/api/Books/" + isbn);
+
+            }
+        }
+    }
+}
---
+++ TCP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 4646);
+            listener.Start();
+            Console.WriteLine("Server ready");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();//Until client comes in, the code doesn't continue
+                Console.WriteLine("Incoming client");
+                Task.Run(() =>
+                {
+                    Client(socket);
+                });
+
+               
+
+
+            }
+        }
+        private static void Client(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            //For more presise use, our reader and writer
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            Client client = new Client();
+            while (true)
+            {
+                switch (reader.ReadLine().ToLower())
+                {
+                    case "getall":
+                        writer.WriteLine(client.GetAll().Result);
+                        break;
+                    case "get":
+                        writer.WriteLine(client.Get(reader.ReadLine()).Result);
+                        break;
+                    case "save":
+                        client.Post(reader.ReadLine());
+                        break;
+                    case "update":
+                        client.Put(reader.ReadLine());
+                        break;
+                    case "delete":
+                        client.Remove(reader.ReadLine());
+                        break;
+
+                }
+            }
+        }
+    }
+}
